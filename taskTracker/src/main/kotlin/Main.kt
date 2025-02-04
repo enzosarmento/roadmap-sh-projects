3f6750b4c5b5101ca36cf4096b254384394f4c16
enzosarmento <enzosarmento91@gmail.com>
2025-02-03 21:41:37 -0300

    refactor: ConsoleView options methods

--- taskTracker/src/main/kotlin/Main.kt
+++ taskTracker/src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 import views.ConsoleView
 
 fun main() {
+    consoleTaskTracker()
+}
 
-    //ConsoleView.option("task-cli add make lunch")
-    //ConsoleView.option("task-cli add make the bed")
-    //ConsoleView.option("task-cli add make dinner")
-    ConsoleView.option("task-cli list ")
-    //ConsoleView.option("task-cli update 1 make lunch")
-    //ConsoleView.option("task-cli mark-in-done 2")
-    //ConsoleView.option("task-cli list")
+private fun consoleTaskTracker() {
+    while (true) {
+        val command = readln()
+        ConsoleView.option(command)
+    }
 }
--- taskTracker/src/main/kotlin/views/ConsoleView.kt
+++ taskTracker/src/main/kotlin/views/ConsoleView.kt
@@ -6,6 +6,7 @@ import models.Status
 import models.Task
 import repositories.TaskRepository
 import services.TaskService
+import kotlin.system.exitProcess
 
 object ConsoleView {
 
@@ -27,76 +28,91 @@ object ConsoleView {
 
     private fun menuOptions(commands: List<String>) {
         when (commands[1]) {
-            "add" -> {
-                var description = ""
-                if (commands.size > 2) {
-                    for (i in 2..<commands.size) {
-                        description += "${commands[i]} "
-                    }
-                    val message = taskController.addTask(description)
-                    println(message)
-                } else {
-                    println("Task description is empty")
-                }
+            "add" -> add(commands)
+            "update" -> update(commands)
+            "delete" -> delete(commands)
+            "mark-in-progress" -> markInProgress(commands)
+            "mark-in-done" -> markInDone(commands)
+            "list" -> list(commands)
+            "exit" -> {
+                println("Task tracker closed")
+                exitProcess(1)
             }
-            "update" -> {
-                var description = ""
-                if (commands.size > 3) {
-                    for (i in 3..<commands.size) {
-                        description += "${commands[i]} "
-                    }
-                    if (isConvertibleToInt(commands[2])) {
-                        println(taskController.updateTask(commands[2].toInt(), description))
-                    } else {
-                        println("ID not specified")
-                    }
-                } else {
-                    println("Task updated description is empty")
-                }
-            }
-            "delete" -> {
-                if (isConvertibleToInt(commands[commands.size - 1])) {
-                    println(taskController.removeTask(commands[commands.lastIndex].toInt()))
-                } else {
-                    println("ID not specified")
-                }
-            }
-            "mark-in-progress" -> {
-                if (commands.size > 2) {
-                    if (isConvertibleToInt(commands[commands.lastIndex])) {
-                        println(taskController.taskStatus(commands[commands.lastIndex].toInt(), Status.PROGRESS))
-                    } else {
-                        println("ID not specified")
-                    }
-                } else {
-                    println("Task mark in progress is empty")
-                }
-            }
-            "mark-in-done" -> {
-                if (commands.size > 2) {
-                    if (isConvertibleToInt(commands[commands.lastIndex])) {
-                        println(taskController.taskStatus(commands[commands.lastIndex].toInt(), Status.DONE))
-                    } else {
-                        println("ID not specified")
-                    }
-                } else {
-                    println("Task mark in done is empty")
-                }
-            }
-            "list" -> {
-                if (commands.size > 2) {
-                    when (commands[commands.lastIndex]) {
-                        "todo" -> taskController.listAllTodo()
-                        "in-progress" -> taskController.listAllInProgress()
-                        "done" -> taskController.listAllDone()
-                        else -> taskController.listAllTasks()
-                    }
-                } else {
-                    taskController.listAllTasks()
-                }
-            }
-            "exit" -> println("Task tracker closed")
             else -> println("Invalid option")
+        }
+    }
+
+    private fun add(commands: List<String>) {
+        var description = ""
+        if (commands.size > 2) {
+            for (i in 2..<commands.size) {
+                description += "${commands[i]} "
+            }
+            val message = taskController.addTask(description)
+            println(message)
+        } else {
+            println("Task description is empty")
+        }
+    }
+
+    private fun update(commands: List<String>) {
+        var description = ""
+        if (commands.size > 3) {
+            for (i in 3..<commands.size) {
+                description += "${commands[i]} "
+            }
+            if (isConvertibleToInt(commands[2])) {
+                println(taskController.updateTask(commands[2].toInt(), description))
+            } else {
+                println("ID not specified")
+            }
+        } else {
+            println("Task updated description is empty")
+        }
+    }
+
+    private fun delete(commands: List<String>) {
+        if (isConvertibleToInt(commands[commands.size - 1])) {
+            println(taskController.removeTask(commands[commands.lastIndex].toInt()))
+        } else {
+            println("ID not specified")
+        }
+    }
+
+    private fun markInProgress(commands: List<String>) {
+        if (commands.size > 2) {
+            if (isConvertibleToInt(commands[commands.lastIndex])) {
+                println(taskController.taskStatus(commands[commands.lastIndex].toInt(), Status.PROGRESS))
+            } else {
+                println("ID not specified")
+            }
+        } else {
+            println("Task mark in progress is empty")
+        }
+    }
+
+    private fun markInDone(commands: List<String>) {
+        if (commands.size > 2) {
+            if (isConvertibleToInt(commands[commands.lastIndex])) {
+                println(taskController.taskStatus(commands[commands.lastIndex].toInt(), Status.DONE))
+            } else {
+                println("ID not specified")
+            }
+        } else {
+            println("Task mark in done is empty")
+        }
+    }
+
+    private fun list(commands: List<String>) {
+        if (commands.size > 2) {
+            when (commands[commands.lastIndex]) {
+                "todo" -> taskController.listAllTodo()
+                "in-progress" -> taskController.listAllInProgress()
+                "done" -> taskController.listAllDone()
+                else -> taskController.listAllTasks()
+            }
+        } else {
+            taskController.listAllTasks()
         }
     }
 
